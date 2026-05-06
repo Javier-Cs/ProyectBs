62a0785230584b2aa5df66ed91961335338ebfd1
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-05-06 00:35:00 -0500

    implementacion de navbar y footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,24 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import path from 'node:path'; 
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['flowbite', 'flowbite-datepicker']
+
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/app/layouts'),
+        '@libs': path.resolve('./src/lib'),
+      }
+    }
   },
 
   integrations: [react()]
---
+++ src/app/constantes.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Prueba';
+export const logosvg = 'https://javier-cs.github.io/Images-Box-Web/images/favicon.ico';
